07d5117fd69e136773aad4740b0a52b749ca555f
BrandonLeone1 <brandonjleone@gmail.com>
2026-05-11 19:27:14 -0400

    Reverting back to previous code

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import cors from 'cors';
 const app = express();
 app.use(cors({
     origin: [
-        'http://localhost:5173/'
+        'http://localhost:5173'
     ],
     credentials: true
 }))
